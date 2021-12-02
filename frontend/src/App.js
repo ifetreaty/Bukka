26d7fa6f1387160580f25a3b9f73a9f720774896
Ifetreaty <ifeoluwaonabanjo@gmail.com>
2021-12-02 17:25:31 +0100

    authentication services

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,70 +1,35 @@
-// import { Outlet, Link } from "react-router-dom";
+import React, { Component } from "react";
 
-// export default function App() {
-//   return (
-//         <div className="App">
-//     <Routes>
-//       <Route exact path="/">
-//         {
-//           user && user._id ? <Homepage/>:<Login/>
-//         }<Homepage/></Route>
-//       {/* <Route path="/Login"><Login setLoginUser={setLoginUser}/></Route> */}
-//       <Route path="/" element={<Homepage />} />
-//       {/* <Route path="/Register"><Register/></Route> */}
-//       <Route path="/Login" element={<Login />} />
-//       <Route path="/Register" element={<Register />} />
-//     </Routes>
-    
-//         </div>
-//       );
-//     }
+class App extends Component {
+  render() {
 
-import './App.css';
-import Homepage from "./components/homepage/Homepage"
-import Login from "./components/login/Login"
-import Register from "./components/register/Register"
-import { Routes, Route, Link } from "react-router-dom";
-import {useState} from 'react';
-function App() {
-  const [user,setLoginUser] = useState({
-
-  })
-  return (
-    <div className="App">
-<Routes>
-  <Route exact path="/">
-    {
-      user && user._id ? <Homepage/>:<Login/>
-    }<Homepage/></Route>
-  {/* <Route path="/Login"><Login setLoginUser={setLoginUser}/></Route> */}
-  <Route path="/" element={<Homepage />} />
-  {/* <Route path="/Register"><Register/></Route> */}
-  <Route path="/Login" element={<Login />} />
-  <Route path="/Register" element={<Register />} />
-</Routes>
-
-    </div>
-  );
+  }
 }
 
 export default App;
+// import './App.css';
+// import Homepage from "./components/homepage/Homepage"
+// import Login from "./components/login/Login"
+// import Register from "./components/register/Register"
+// import { Routes, Route } from "react-router-dom";
+// import {useState} from 'react';
+// function App() {
+//   const [user,setLoginUser] = useState({
 
-// import { Outlet, Link } from "react-router-dom";
-
-// export default function App() {
+//   })
 //   return (
-//     <div>
-//       <h1>Bookkeeper!</h1>
-//       <nav
-//         style={{
-//           borderBottom: "solid 1px",
-//           paddingBottom: "1rem"
-//         }}
-//       >
-//         <Link to="/register">Create Account</Link> |{" "}
-//         <Link to="/login">Login</Link>
-//       </nav>
-//       <Outlet />
+//     <div className="App">
+// <Routes>
+//   <Route exact path="/">
+//     {
+//       user && user._id ? <Homepage/>:<Login/>
+//     }<Homepage/></Route>
+//   <Route path="/Login"><Login setLoginUser={setLoginUser}/></Route>
+//   <Route path="/Register"><Register/></Route>
+// </Routes>
+
 //     </div>
 //   );
 // }
+
+// export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
+
 // import * as React from "react";
 // import * as ReactDOM from "react-dom";
 // import { BrowserRouter } from "react-router-dom";
@@ -32,45 +48,45 @@
 // reportWebVitals();
 
 
-import { render } from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import App from "./App";
-import Homepage from "./components/homepage/Homepage";
-import Login from "./components/login/Login";
-import Register from "./components/register/Register";
+// import { render } from "react-dom";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+// import App from "./App";
+// import Homepage from "./components/homepage/Homepage";
+// import Login from "./components/login/Login";
+// import Register from "./components/register/Register";
 
-const rootElement = document.getElementById("root");
-render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} >
-        <Route path="homepage" element={<Homepage />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-          {/* <Route
-            index //this is index and not "path" because it shares the path of the parent
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>Select an invoice</p>
-              </main>
-            }
-          />
-          <Route path=":invoiceId" element={<Invoice />} />
-        </Route>
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>There's nothing here!</p>
-            </main>
-        }
-      /> */}
-      </Route>
-    </Routes>
-  </BrowserRouter>,
-  rootElement
-);
+// const rootElement = document.getElementById("root");
+// render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<App />} >
+//         <Route path="homepage" element={<Homepage />} />
+//         <Route path="login" element={<Login />} />
+//         <Route path="register" element={<Register />} />
+//           {/* <Route
+//             index //this is index and not "path" because it shares the path of the parent
+//             element={
+//               <main style={{ padding: "1rem" }}>
+//                 <p>Select an invoice</p>
+//               </main>
+//             }
+//           />
+//           <Route path=":invoiceId" element={<Invoice />} />
+//         </Route>
+//         <Route
+//           path="*"
+//           element={
+//             <main style={{ padding: "1rem" }}>
+//               <p>There's nothing here!</p>
+//             </main>
+//         }
+//       /> */}
+//       </Route>
+//     </Routes>
+//   </BrowserRouter>,
+//   rootElement
+// );
