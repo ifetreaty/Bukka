24b0ac6990c8be804a4d1d9efbcce81e44425ab4
Ifetreaty <ifeoluwaonabanjo@gmail.com>
2022-04-03 03:58:50 +0100

    Meals page no longer rendering on browser

--- frontend/src/components/MealComponents/TotalMeals.component.js
+++ frontend/src/components/MealComponents/TotalMeals.component.js
@@ -1,5 +1,5 @@
 import React, { Component, useEffect, useState } from "react";
-import axios from "axios";
+
 import "../../App.css";
 import mealService from "../../services/meal.service";
 import MealCard from "./meal-card.component";
@@ -8,69 +8,66 @@ import EditButton from "./edit.component";
 import DeleteButton from "./delete.component";
 import SelectMeal from "./select-meal.component";
 
-export default class TotalMeals extends Component {
-  constructor(props) {
-    super(props);
+const TotalMeals = () => {
+  const [meals, setMeals] = useState([]);
 
-    this.state = {
-      meals: [],
-      errorMessage: "",
-    };
-  }
+  useEffect(() => {
+    fetchMeals();
+  }, []);
 
-  componentDidMount() {
-    mealService.getMeals().then((res) => {
-      console.log(res);
-      this.setState({
-        meals: res.data,
+  const fetchMeals = () => {
+    mealService
+      .getMeals()
+      .then((res) => {
+        console.log(res);
+        setMeals(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
       });
-    });
-  }
+  };
 
-  removeMeal(id, e) {
+  const removeMeal = (id) => {
     mealService.deleteMeal(id).then((res) => {
-      this.setState({
-        meals: this.state.meals.filter((meal) => meal._id !== id),
-      });
+      setMeals((meals) => meals.filter((meal) => meal._id !== id));
     });
-  }
+  };
 
-  render() {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <h1 className="meal-list-title">Meal List</h1>
-        </div>
-        <div className="cards">
-          {this.state.meals.map((meal) => {
-            return (
-              <div className="meal-card">
-                <MealCard
-                  key={meal._id}
-                  image={meal.image}
-                  name={meal.name}
-                  description={meal.description}
-                  price={meal.price}
-                />
-                <div className="meal-card-body">
-                  <SelectMeal />
-                </div>
-                <div className="meal-card-edit-icon">
-                  <Link to={`/admin/meals/edit/${meal._id}`}>
-                    <EditButton />
-                  </Link>
-                </div>
-                <div
+        <h1 className="meal-list-title">Meal List</h1>
+      </div>
+      <div className="cards">
+        {meals.map((meal) => (
+          <div className="meal-card">
+            <MealCard
+              key={meal._id}
+              image={meal.image}
+              name={meal.name}
+              description={meal.description}
+              price={meal.price}
+            />
+            <div className="meal-card-body">
+              <SelectMeal />
+            </div>
+            <div className="meal-card-edit-icon">
+              <Link to={`/admin/meals/edit/${meal._id}`}>
+                <EditButton />
+              </Link>
+            </div>
+            <div
                   className="meal-card-delete-icon"
-                  onClick={(e) => this.removeMeal(meal._id, e)}
+                  onClick={(e) => removeMeal(meal._id, e)}
                 >
                   <DeleteButton />
                 </div>
-              </div>
-            );
-          })}
-        </div>
+          </div>
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default TotalMeals;
+
