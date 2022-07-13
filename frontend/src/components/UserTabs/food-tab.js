3513e13d95286b16da433f1fe1015000c85803dd
Ifetreaty <ifeoluwaonabanjo@gmail.com>
2022-07-13 10:26:59 +0100

    first commit to test heroku deployment

---
+++ frontend/src/components/UserTabs/food-tab.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useMemo } from "react";
+import menuService from "../../services/menu.service";
+import MealCard from "../MealComponents/meal-card.component";
+import SelectMeal from "../User/MenuComponents/add-to-cart.component";
+import { FaRegTrashAlt } from "react-icons/fa";
+
+function FoodTab() {
+  const [menuItems, setMenuItems] = useState([]);
+  const [foodCategory, setFoodCategory] = useState();
+
+  useEffect(() => {
+    setFoodCategory(fetchMenuItems);
+  }, []);
+
+  const fetchMenuItems = () => {
+    const id = "6231da8ecece324534b292da";
+    menuService
+      .getMenuItemsByCategory(id)
+      .then((res) => {
+        console.log(res);
+        setMenuItems(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  function getFoodCategory() {
+    if (!foodCategory) {
+      return menuItems;
+    }
+    return menuItems.filter((menuItem) => menuItem.category === foodCategory);
+  }
+
+  let foodCategoryList = useMemo(getFoodCategory, [foodCategory, menuItems]);
+
+  const removeMeal = (id) => {
+    menuService.deleteMenuItem(id).then((res) => {
+      setMenuItems((menuItem) => menuItem?.filter((meal) => meal._id !== id));
+    });
+  };
+
+  return (
+    <div className="cards">
+      {foodCategoryList?.map((menuitem) => (
+        <div className="meal-card">
+          <MealCard
+            key={menuitem._id}
+            image={menuitem.meal.image}
+            name={menuitem.meal.name}
+            description={menuitem.meal.description}
+            price={menuitem.meal.price}
+          />
+          {/* <div
+            className="meal-card-delete-menu"
+            onClick={(e) => removeMeal(menuitem._id, e)}
+          >
+            <FaRegTrashAlt />
+          </div> */}
+          <div className="meal-card-body">
+              <SelectMeal mealId={menuitem._id} />
+            </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default FoodTab;
